047109404364ec5e8c34c15bb2c650cb44fc59e9
Luis Alfredo Carmona Martínez <acarmonacuarto@gmail.com>
2024-08-18 23:15:45 -0600

    Created a Vite Project
    Here's the code of the new buttons and how react Fragment allows to wrap them all up

---
+++ 00-hola-mundo/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+
+const root = ReactDom.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.Fragment>
+    <button>Hola botón</button>
+    <button>Hola botón</button>
+    <button>Hola botón</button>
+  </React.Fragment>)

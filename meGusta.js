61845a32df2670cbe250918cc272d071909498f6
Luis Alfredo Carmona Martínez <acarmonacuarto@gmail.com>
2024-08-18 22:22:28 -0600

    Buttons with React
    Here I use React to recycle the button element, but it still not perfect since I need to create a new constant for every single button

--- meGusta.js
+++ meGusta.js
@@ -1,19 +1,18 @@
-// Recuperar el botón
-const buttons = document.querySelectorAll('button')
+import React from 'https://esm.sh/react@18.2.0'
+import ReactDOM from 'https://esm.sh/react-dom@18.2.0/client'
 
-buttons.forEach(button => {
-	// Al hacer clic en el botón, ejecutar una función
-	button.addEventListener('click', function() {
-		// Recuperar el id del atributo de HTML
-		const id = button.getAttribute('data-id')
-		
-		// Alternar "me gusta" y "quitar me gusta"
-		if (button.classList.contains('liked')) {
-			button.classList.remove('liked')  // Fix: 'Remove' -> 'remove'
-			button.innerText = 'Me gusta'
-		} else {
-			button.classList.add('liked')  // Fix: 'Liked' -> 'liked'
-			button.innerText = 'Quitar me gusta'
-		}
-	});
-});
+const appDomElement = document.getElementById('app')
+
+const root = ReactDOM.createRoot(appDomElement)
+
+const h = React.createElement
+
+// Create button without any event handler
+const button = h('button', { 'data-id': 123}, 'Button 1')
+const button2 = h('button', { 'data-id': 456}, 'Button 2')
+const button3 = h('button', { 'data-id': 789}, 'Button 3')
+
+const app   = h(React.Fragment, null, [button, button2, button3])
+// Fragment es utilizado para no renderizar elementos vacios
+
+root.render(app)

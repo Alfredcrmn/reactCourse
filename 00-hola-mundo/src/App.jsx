545457c87b8d05bda506713c516d9907c7c04f1c
Luis Alfredo Carmona Martínez <acarmonacuarto@gmail.com>
2024-08-19 22:57:54 -0600

    Created the component
    Created the component of the user's profile. Creating also a App.jsx file to export the function and render it in main.jsx

---
+++ 00-hola-mundo/src/App.jsx
@@ -0,0 +1,21 @@
+export function App() {
+  return (
+    <div>
+      <article>
+        <header>
+            <img src="https://unavatar.io/google/netflix.com" alt="Netflix avatar" />
+            <div>
+                <strong>Netflix</strong>
+                <span>@Netflix.us</span>
+            </div>
+        </header>
+
+        <aside>
+            <button>
+                Seguir
+            </button>
+        </aside>
+      </article>
+    </div>
+  );
+}
